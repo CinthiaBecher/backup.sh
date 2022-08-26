7135bc95490e4d0e6af6a760373b92e9ef939414
Cinthia Becher <63256286+CinthiaBecher@users.noreply.github.com>
2022-08-26 16:23:58 -0300

    Update

--- backup.sh
+++ backup.sh
@@ -1,32 +1,35 @@
 #!/bin/bash
 
-date=$(date +%d-%m-%y)
+
+data=$(date +%d-%m-%y)
 
 #Solicita diretório para Backup
 echo  'Digite o caminho do diretório para a realização do backup'
 read caminho
 
 #Criando arquivo log
-arquivo_log="/home/cinthia/Backup/logBKP.log"
+arquivo_log="/home/linux/Backup/logBKP.log"
 exec 1> $arquivo_log
 
 #Guardando informações Backup
-echo -e  "Execução do Backup - $data"
+echo -e  "\nExecução do Backup - $data"
 
 echo -e 'Horário de Início - \c'
 date +%H:%M:%S
 
-pasta_backup='/home/cinthia/Backup'
+pasta_backup='/home/linux/Backup'
 
 #Criando arquivo backup
 arquivo_backup="backup_$data.tgz"
 
 #Realizando Backup
-echo 'Arquivos Compactados' 
+echo -e "\nArquivos Compactados" 
 tar -czSpf "$pasta_backup/$arquivo_backup" "$caminho" >> $arquivo_log
 
+cd $pasta_backup
+tar -tz -f $arquivo_backup 
 
 #Guardando informações de finalizacao do backup
-echo "Diretório de backup: $pasta_backup"
+echo -e  "\nDiretório de backup: $pasta_backup"
 echo -e 'Horário de Finalização do Backup - \c'
 date +%H:%M:%S
